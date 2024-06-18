32fd5d8cf0884dd23076502fe5e9e6bff281cd47
Victor Martinez <victormartinezrubio@gmail.com>
2024-06-18 14:39:04 +0200

    feat: parse junit files (#3)

--- receiver.go
+++ receiver.go
@@ -5,17 +5,21 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"github.com/google/go-github/v62/github"
-	"github.com/julienschmidt/httprouter"
-	"go.opentelemetry.io/collector/component"
-	"go.opentelemetry.io/collector/consumer"
-	"go.opentelemetry.io/collector/receiver"
-	"go.uber.org/zap"
 	"io"
 	"net/http"
 	"os"
 	"path/filepath"
 	"strings"
+
+	"github.com/google/go-github/v62/github"
+	"github.com/joshdk/go-junit"
+	"github.com/julienschmidt/httprouter"
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/consumer"
+	"go.opentelemetry.io/collector/receiver"
+	"go.opentelemetry.io/otel/attribute"
+	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
+	"go.uber.org/zap"
 )
 
 func newTracesReceiver(cfg *Config, params receiver.CreateSettings, nextConsumer consumer.Traces) (receiver.Traces, error) {
@@ -120,6 +124,7 @@ func (rec *githubactionsjunitReceiver) handleWorkflowRunEvent(workflowRunEvent *
 	for _, artifact := range junitArtifacts {
 		err := processArtifact(rec.logger, rec.ghClient, workflowRunEvent, artifact)
 		if err != nil {
+			// TODO: report error but keep processing other artifacts
 			rec.logger.Error("Failed to process artifact", zap.Error(err))
 			w.WriteHeader(http.StatusInternalServerError)
 			return
@@ -135,10 +140,88 @@ func processArtifact(logger *zap.Logger, ghClient *github.Client, workflowRunEve
 	}
 	defer zipFile.Close()
 	for _, file := range zipFile.Reader.File {
-		fileName := file.Name
-		logger.Debug("Processing file", zap.String("artifact", artifact.GetName()), zap.String("file", fileName))
+		logger.Debug("Processing file", zap.String("artifact", artifact.GetName()), zap.String("file", file.Name))
+		suites := processJunitFile(file, logger)
+		for _, suite := range suites {
+			processSuite(suite, logger)
+		}
 	}
 	return nil
+}
+
+func processJunitFile(file *zip.File, logger *zap.Logger) []junit.Suite {
+	fileName := file.Name
+	src, err := file.Open()
+	if err != nil {
+		logger.Error("Failed to open file in zip", zap.Error(err))
+	}
+	defer src.Close()
+
+	dst, err := os.Create(filepath.Join("/tmp", fileName))
+	if err != nil {
+		logger.Error("Failed to create destination file", zap.Error(err))
+	}
+	defer dst.Close()
+
+	// TODO: optimise if the file is too big.
+	contents, err := io.ReadAll(src)
+	if err != nil {
+		logger.Error("Failed to read file content", zap.Error(err))
+	}
+	suites, err := junit.Ingest(contents)
+	if err != nil {
+		logger.Error("Failed to ingest JUnit file", zap.Error(err))
+	}
+	return suites
+}
+
+func processSuite(suite junit.Suite, logger *zap.Logger) {
+
+	// Set up the attributes for the suite
+	suiteAttributes := []attribute.KeyValue{
+		semconv.CodeNamespaceKey.String(suite.Package),
+		attribute.Key(TestsSuiteName).String(suite.Name),
+		attribute.Key(TestsSystemErr).String(suite.SystemErr),
+		attribute.Key(TestsSystemOut).String(suite.SystemOut),
+		attribute.Key(TestsDuration).Int64(suite.Totals.Duration.Milliseconds()),
+	}
+
+	// Add suite properties as labels
+	suiteAttributes = append(suiteAttributes, propsToLabels(suite.Properties)...)
+
+	// For each test in the suite, set up the attributes
+	for _, test := range suite.Tests {
+		testAttributes := []attribute.KeyValue{
+			semconv.CodeFunctionKey.String(test.Name),
+			attribute.Key(TestDuration).Int64(test.Duration.Milliseconds()),
+			attribute.Key(TestClassName).String(test.Classname),
+			attribute.Key(TestMessage).String(test.Message),
+			attribute.Key(TestStatus).String(string(test.Status)),
+			attribute.Key(TestSystemErr).String(test.SystemErr),
+			attribute.Key(TestSystemOut).String(test.SystemOut),
+		}
+
+		testAttributes = append(testAttributes, propsToLabels(test.Properties)...)
+		testAttributes = append(testAttributes, suiteAttributes...)
+
+		if test.Error != nil {
+			testAttributes = append(testAttributes, attribute.Key(TestError).String(test.Error.Error()))
+		}
+		var stringSlice []string
+		for _, attr := range testAttributes {
+			stringSlice = append(stringSlice, fmt.Sprintf("%s: %v", attr.Key, attr.Value.AsString()))
+		}
+		logger.Debug("Processing test suite", zap.Strings("attributes", stringSlice))
+	}
+}
+
+func propsToLabels(props map[string]string) []attribute.KeyValue {
+	attributes := []attribute.KeyValue{}
+	for k, v := range props {
+		attributes = append(attributes, attribute.Key(k).String(v))
+	}
+
+	return attributes
 }
 
 func getArtifacts(ctx context.Context, ghEvent *github.WorkflowRunEvent, ghClient *github.Client) ([]*github.Artifact, error) {
